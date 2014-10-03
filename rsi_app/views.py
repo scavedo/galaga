442f79d992ee23ad8cda1d64ee1fa6f205c6f18d
Trent Holliday <trumpet2012@gmail.com>
2014-10-03 13:21:39 -0400

    Updated views and urls

--- rsi_app/views.py
+++ rsi_app/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rsi_app.models import Founders
 
 # Create your views here.
 def index(request):
-        # now return the rendered template
-        return render(request, 'rsi_app/index.html')
+    return render(request, 'rsi_app/index.html')\
+
+def founders_list(request):
+    founders = Founders.objects.all()
+    return render(request, 'rsi_app/founders_list.html', {'founders': founders})
+
+def founder_detail(request, slug):
+    founder = get_object_or_404(Founders, slug=slug)
+    return render(request, 'rsi_app/founder_detail.html', {'founder': founder})
