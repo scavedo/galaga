f5c7110a031c01b20ceab3f4fd45204a41e32085
charles <j95.charles@gmail.com>
2014-10-03 13:36:26 -0400

    urls

--- galaga/urls.py
+++ galaga/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rsi_app.views import *
 
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','rsi_app.views.index'),
-    url(r'^(?P<slug>[\w\-]+)/$', 'rsi_app.views.founders_detail'),
+    url(r'^(?P<slug>[\w\-]+)/$', founder_detail),
+    url(r'^founders', founders_list)
 )
