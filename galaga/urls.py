ebe3404645e0c14f61586ce1e06078d2757465c4
charles <j95.charles@gmail.com>
2014-10-03 13:38:08 -0400

    admin works now

--- galaga/urls.py
+++ galaga/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rsi_app.views import *
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
