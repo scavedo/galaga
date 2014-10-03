4e685665f9d47862e57eaa442213e5864d1eb606
scavedo159 <scavedo159@gmail.com>
2014-10-03 13:06:31 -0400

    base template

--- rsi_app/models.py
+++ rsi_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Founders(models.Model):
     name = models.CharField(max_length=256)
-    birth_year = models.PositiveIntegerFieldIntegerField(default=1700)
-    death_year = models.PositiveIntegerFieldIntegerField(default=1700)
+    birth_year = models.PositiveIntegerField(default=1700)
+    death_year = models.PositiveIntegerField(default=1700)
     position = models.CharField(max_length=256)
     slug = models.SlugField(max_length=256)
 
