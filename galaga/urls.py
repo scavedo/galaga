04fa6244bca57650f8ba6219ae7048063f8acf1c
Trent Holliday <trumpet2012@gmail.com>
2014-10-03 12:50:00 -0400

    Updated views and urls, created templates for founders.

--- galaga/urls.py
+++ galaga/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','rsi_app.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'rsi_app.views.founders_detail'),
 )
